2807d1397abd9de3bad98dacf7620fe5321e8457
kolya5544 <kolya@iktm.me>
2026-03-14 17:42:17 +0300

    5.15.0rc1 update, Python 3.6 to 3.9 support dropped (EOL)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='pybit',
-    version='5.14.0',
+    version='5.15.0rc1',
     description='Python3 Bybit HTTP/WebSocket API Connector', 
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,12 +21,12 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords="bybit api connector",
     packages=["pybit"],
-    python_requires=">=3.6",
+    python_requires=">=3.10",
     install_requires=[
         "requests",
         "websocket-client",
